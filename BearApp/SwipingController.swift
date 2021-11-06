2e068f2039cd889c4cd14fc2726a69a08e1b0b6c
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-11-06 20:34:43 +0300

    final

--- BearApp/SwipingController.swift
+++ BearApp/SwipingController.swift
@@ -93,6 +93,22 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         pageControl.currentPage = Int(x / view.frame.width)
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate { (_) in
+            self.collectionViewLayout.invalidateLayout()
+            
+            if self.pageControl.currentPage == 0 {
+                self.collectionView.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+        } completion: { (_) in
+        
+        }
+
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         pages.count
     }
